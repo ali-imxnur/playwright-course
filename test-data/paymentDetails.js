325e8b39c27995ea4bdba4bcac81ce0924d80f77
ali-imxnur <imanur.8670@gmail.com>
2025-11-06 03:57:19 +0530

    Updated package.json

---
+++ test-data/paymentDetails.js
@@ -0,0 +1,6 @@
+export const paymentDetails = {
+    owner : "John Ckis",
+    creditCard : "1234123412341234",
+    validUntil : "12/30",
+    cvc : "321"
+}
---
+++ test-data/userDetails.js
@@ -0,0 +1,8 @@
+export const userDetails = {
+    firstName : "Tester",
+    lastName : "LTester",
+    street : "Something 123 Rv",
+    postalcode : "12345",
+    city : "coochbehar",
+    country : "India"
+}
